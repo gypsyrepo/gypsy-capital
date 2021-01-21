ff4cc011742ec08de5564fb8ba67ede86ba785f3
Rouche01 <richardemate@gmail.com>
2021-01-22 00:20:34 +0100

    feat: added sidebar routes to dashboard

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -5,15 +5,44 @@ import Logo from '../../assets/logo-white.png';
 import Button from '../../components/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
+import { Link } from 'react-router-dom';
+import pageUrl from '../../routes/pageUrl';
+import { FiLayers } from 'react-icons/fi';
+import { BiCreditCard } from 'react-icons/bi';
+import { AiOutlineUser } from 'react-icons/ai';
+import { GiTakeMyMoney } from 'react-icons/gi';
 
 
-const Profile = () => {
+const Profile = ({ location }) => {
+
+  console.log(location);
+
   return(
     <div className={styles.container}>
       <Row>
         <Col className={styles.menuPanel} sm={3}>
           <div className={styles.header}>
             <img src={Logo} alt="Gypsy Logo" width={150} />
+          </div>
+          <div className={styles.menuList}>
+            <ul>
+              <li className="mb-4">
+                <FiLayers size="1.3em" color="#fff" />
+                <Link to={pageUrl.DASHBOARD_HOMEPAGE}>Dashboard</Link>
+              </li>
+              <li className="mb-4">
+                <GiTakeMyMoney size="1.3em" color="#fff" />
+                <Link to={pageUrl.CONSUMER_CREDIT_PAGE}>Consumer Credit</Link>
+              </li>
+              <li className="mb-4">
+                <BiCreditCard  size="1.3em" color="#fff" />
+                <Link to={pageUrl.CREDIT_REPORT_PAGE}>Credit Report</Link>
+              </li>
+              <li className="mb-4">
+                <AiOutlineUser  size="1.3em" color="#fff" />
+                <Link to={pageUrl.PROFILE_PAGE}>Profile</Link>
+              </li>
+            </ul>
           </div>
         </Col>
         <Col sm={9} className={styles.mainPanel}>
--- src/routes/pageUrl.js
+++ src/routes/pageUrl.js
@@ -4,6 +4,9 @@ const SIGNIN_PAGE = '/signin';
 const TERMS_CONDITIONS_PAGE='/terms-conditions';
 const PRIVACY_POLICY_PAGE='/privacy';
 const PROFILE_PAGE='/dashboard/profile';
+const DASHBOARD_HOMEPAGE='/dashboard/overview';
+const CONSUMER_CREDIT_PAGE='/dashboard/consumer-credit';
+const CREDIT_REPORT_PAGE= '/dashboard/credit-report';
 
 
 const pageUrl = {
@@ -12,7 +15,10 @@ const pageUrl = {
     SIGNIN_PAGE,
     TERMS_CONDITIONS_PAGE,
     PRIVACY_POLICY_PAGE,
-    PROFILE_PAGE
+    PROFILE_PAGE,
+    DASHBOARD_HOMEPAGE,
+    CONSUMER_CREDIT_PAGE,
+    CREDIT_REPORT_PAGE
 }
 
 export default pageUrl;
