15dfa4be0b8bd581a4270f7afd81d9501f039bbe
Rouche01 <richardemate@gmail.com>
2021-02-24 06:12:05 +0100

    feat: api integration for overview

---
+++ src/routes/protectedroute/SalesRoute.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { Route, Redirect, useLocation } from 'react-router-dom';
+import { Context as AuthContext } from '../../context/AuthContext';
+import pageUrl from '../pageUrl';
+
+
+const SalesRoute = () => {
+  const { state: { loggedIn, user } } = useContext(AuthContext);
+  const location = useLocation()
+
+  return (
+    <Route {...rest}>
+      { loggedIn && user.role === 'sales' ? 
+          <Component /> :
+          <Redirect to={{ pathname: pageUrl.SIGNIN_PAGE, state: { from: location } }} />
+      }
+    </Route>
+  )
+}
+
+
+export default SalesRoute;
