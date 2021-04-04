f54bb96f3771d518babb541d9f58e74c2984c03f
Richard Emate <richardemate@gmail.com>
2021-04-05 00:17:04 +0100

    false edit

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,41 +1,38 @@
-import { getDefaultNormalizer } from '@testing-library/react';
-import React from 'react';
-import styles from './Button.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { getDefaultNormalizer } from "@testing-library/react";
+import React from "react";
+import styles from "./Button.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
-
-const Button = ({ 
-  children, 
-  className, 
-  fullWidth, 
-  clicked, 
-  bgColor, 
-  size, 
-  color, 
-  disabled, 
-  loading 
+const Button = ({
+  children,
+  className,
+  fullWidth,
+  clicked,
+  bgColor,
+  size,
+  color,
+  disabled,
+  loading,
 }) => {
-
   const buttonStyle = {
-    width: fullWidth ? '100%' : getDefaultNormalizer,
+    width: fullWidth ? "100%" : getDefaultNormalizer,
     backgroundColor: bgColor,
-    padding: size === 'lg' ? '12px 28px' : '8px 28px',
-    color: color
-  }
+    padding: size === "lg" ? "12px 28px" : "8px 28px",
+    color: color,
+  };
 
   return (
-    <button 
-      className={[className, styles.button].join(' ')}
+    <button
+      className={[className, styles.button].join(" ")}
       style={buttonStyle}
       onClick={clicked}
       disabled={disabled}
     >
-      { loading && <FontAwesomeIcon icon={faSpinner} className="fa-spin mr-3" /> }
-      { loading ? 'Loading...' : children }
+      {loading && <FontAwesomeIcon icon={faSpinner} className="fa-spin mr-3" />}
+      {loading ? "Loading..." : children}
     </button>
-  )
-}
-
+  );
+};
 
 export default Button;
