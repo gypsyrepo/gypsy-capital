00632ed6bac0f1f38ad19edce47d09fe66271df1
Babatunde Dawodu <pintop.web.software@outlook.com>
2021-09-21 22:13:56 +0100

    Debugging

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Registration = () => {
   return (
-    <iframe title="Registration" src="https://docs.google.com/forms/d/e/1FAIpQLScUkvIcS0dTKKZ665T_DrumgM2TbFbOjH_88WyUdh8xEr7rxw/viewform?embedded=true" width="640" height="1972" frameborder="0" marginheight="0" marginwidth="0" sandbox=''/>
+    // <iframe title="Registration" src="https://docs.google.com/forms/d/e/1FAIpQLScUkvIcS0dTKKZ665T_DrumgM2TbFbOjH_88WyUdh8xEr7rxw/viewform?embedded=true" width="640" height="1972" frameborder="0" marginheight="0" marginwidth="0" sandbox=''/>
+    <h1>I worked here and now</h1>
   )
 }
 
